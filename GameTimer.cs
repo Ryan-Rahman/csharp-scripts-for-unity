a6c314e571e0ef3eee635a261acd6293a517057d
Ryan-Rahman <74671082+Ryan-Rahman@users.noreply.github.com>
2020-12-15 16:48:06 -0500

    GameTimer.cs

---
+++ GameTimer.cs
@@ -0,0 +1,19 @@
+//simple timed game for 290 seconds, customizable
+using UnityEngine;
+using UintyEngine.SceneManagement;
+
+public class GameTimer : MonoBehaviour {
+    //how much time is left
+    public float timeLeft = 290f;
+    //text that will display timer
+    public Text startText;
+
+    //update method starts timer
+    void Update()
+    {
+        timeLeft = Time.deltaTime;
+        startText.text = (timeLeft).ToString("0");
+        if (timeLeft < 0)
+        {
+      
+}
